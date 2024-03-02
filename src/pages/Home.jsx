690fa44c53acbb2ec88bbd5ad72fa37b61d8e2bd
Anup Khismatrao <khismatrao2003@gmail.com>
2024-03-02 18:28:22 +0530

    Add Menu and Toolbox UI to Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Menu from "../components/Menu/Menu";
+import Toolbox from "../components/Toolbox/Toolbox";
+
+const Home = () => {
+  return (
+    <>
+      <Menu />
+      <Toolbox />
+    </>
+  );
+};
+
+export default Home;
