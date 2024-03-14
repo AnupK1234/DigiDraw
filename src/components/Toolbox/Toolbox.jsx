a03aa3c54e244d2b1e283110ea26eea915719548
Anup Khismatrao <khismatrao2003@gmail.com>
2024-03-14 13:05:56 +0530

    Implemented Redux-based communication between the Toolbox and Menu components

--- src/components/Toolbox/Toolbox.jsx
+++ src/components/Toolbox/Toolbox.jsx
@@ -1,46 +1,59 @@
 import React from "react";
 import styles from "./toolbox.module.css";
-import { COLORS } from "../../constants";
+import { COLORS, MENU_ITEMS } from "../../constants";
+import { useSelector } from "react-redux";
 
 const Toolbox = () => {
   const updateBrushSize = (e) => {};
+
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+
+  const showStrokeOption = activeMenuItem === MENU_ITEMS.PENCIL;
+  const showToolOption =
+    activeMenuItem === MENU_ITEMS.PENCIL || MENU_ITEMS.ERASER;
+
   return (
     <div className={styles.toolboxContainer}>
-      <div className={styles.toolItem}>
-        <h4 className={styles.toolText}>Stroke Color</h4>
-        <div className={styles.itemContainer}>
-          <div
-            className={styles.colorBox}
-            style={{ backgroundColor: COLORS.BLACK }}
-          />
-          <div
-            className={styles.colorBox}
-            style={{ backgroundColor: COLORS.RED }}
-          />
-          <div
-            className={styles.colorBox}
-            style={{ backgroundColor: COLORS.GREEN }}
-          />
-          <div
-            className={styles.colorBox}
-            style={{ backgroundColor: COLORS.BLUE }}
-          />
-          <div
-            className={styles.colorBox}
-            style={{ backgroundColor: COLORS.ORANGE }}
-          />
-          <div
-            className={styles.colorBox}
-            style={{ backgroundColor: COLORS.YELLOW }}
-          />
+      {showStrokeOption && (
+        <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Stroke Color</h4>
+          <div className={styles.itemContainer}>
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.BLACK }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.RED }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.GREEN }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.BLUE }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.ORANGE }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.YELLOW }}
+            />
+          </div>
         </div>
-      </div>
-      <div className={styles.toolItem}>
-        <h4 className={styles.toolText}>Brush Size</h4>
-        <div className={styles.itemContainer}>
-          <input type="range" min={1} max={10} onChange={updateBrushSize} />
+      )}
+
+      {showToolOption && (
+        <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Brush Size</h4>
+          <div className={styles.itemContainer}>
+            <input type="range" min={1} max={10} onChange={updateBrushSize} />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Menu from "../components/Menu/Menu";
 import Toolbox from "../components/Toolbox/Toolbox";
+import Board from "../components/Board/Board";
 
 const Home = () => {
   return (
     <>
       <Menu />
       <Toolbox />
+      <Board />
     </>
   );
 };
